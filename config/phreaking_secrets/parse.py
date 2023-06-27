e6597101e72291641c9e3df79614d2a0f1551d7b
theodorsm <theodor@midtlien.com>
2023-06-27 17:35:26 +0200

    Add parser

---
+++ config/phreaking_secrets/parse.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.argv[1])
+
+parsed_env_secrets = []
+
+for i in range(1, n + 1):
+    with open(f"./team{i}.phreaking.secrets.txt") as f:
+        lines = [line for line in f]
+        parsed_env_secrets.append(lines[0].replace("PHREAKING_", f"PHREAKING_{i}_"))
+        parsed_env_secrets.append(lines[1].replace("PHREAKING_", f"PHREAKING_{i}_"))
+
+
+with open("checker_env.txt", "w") as fp:
+    fp.writelines(parsed_env_secrets)
