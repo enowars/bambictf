8cecea5c04d12576d9c8ddc985c3e3d94ef4fc9c
Benedikt Radtke <benediktradtke@gmail.com>
2023-11-19 23:12:53 +0100

    fix arkime viewer connectivity (but not funktionality -.-)

--- ansible/roles/bambi-arkime/files/arkime-viewer.sh
+++ ansible/roles/bambi-arkime/files/arkime-viewer.sh
@@ -2,6 +2,6 @@ cd /opt/arkime/viewer
 while :
 do
   echo "Starting Arkime viewer"
-  /opt/arkime/bin/node viewer.js -c /opt/arkime/etc/config.ini --host $1
+  /opt/arkime/bin/node viewer.js -c /opt/arkime/etc/config.ini --host $1 -n $1
   sleep 5
 done
