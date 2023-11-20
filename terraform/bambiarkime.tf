6f8c71ebd93a65cc1e7274be4d6288494ef0f7b9
Benedikt Radtke <benediktradtke@gmail.com>
2023-11-20 12:21:48 +0100

    start arkime automagically, make arkime truly optional

--- terraform/bambiarkime.tf
+++ terraform/bambiarkime.tf
@@ -4,9 +4,9 @@ variable "arkime_type" {
   nullable  = false
 }
 
-variable "arkime_count" {
-  type      = number
-  default   = 0
+variable "use_arkime" {
+  type      = bool
+  default   = false
   nullable  = false
 }
 
@@ -15,13 +15,13 @@ locals {
 }
 
 data "hcloud_image" "bambiarkime" {
-  with_selector = var.arkime_count > 0 ? "type=bambiarkime" : null
-  name          = var.arkime_count > 0 ? null : "debian-10"
+  with_selector = var.use_arkime ? "type=bambiarkime" : null
+  name          = var.use_arkime ? null : "debian-10"
   most_recent   = true
 }
 
 resource "hetznerdns_record" "bambarkime_dns" {
-  count   = var.hetznerdns_zone != null ? var.arkime_count : 0
+  count   = var.hetznerdns_zone != null && var.use_arkime ? var.router_count : 0
   zone_id = data.hetznerdns_zone.zone[0].id
   name    = "arkime${count.index + 1}${local.subdomain}"
   value   = hcloud_server.bambiarkime[count.index].ipv4_address
@@ -30,7 +30,7 @@ resource "hetznerdns_record" "bambarkime_dns" {
 }
 
 resource "hcloud_server" "bambiarkime" {
-  count       = var.arkime_count
+  count       = var.use_arkime ? var.router_count : 0
   name        = "arkime${count.index + 1}"
   image       = data.hcloud_image.bambiarkime.id
   location    = var.home_location
@@ -40,8 +40,8 @@ resource "hcloud_server" "bambiarkime" {
   user_data = templatefile(
     "user_data_arkime.tftpl", {
       id          = "${count.index + 1}",
-      masters     = join(",", [for i in range(var.arkime_count) : cidrhost(local.subnet, i+1)]),
-      seeds       = join(",", setsubtract([for i in range(var.arkime_count) : cidrhost(local.subnet, i+1)], [cidrhost(local.subnet, count.index+1)])) 
+      masters     = join(",", [for i in range(var.router_count) : cidrhost(local.subnet, i+1)]),
+      seeds       = join(",", setsubtract([for i in range(var.router_count) : cidrhost(local.subnet, i+1)], [cidrhost(local.subnet, count.index+1)])) 
       router_ips  = hcloud_floating_ip.bambirouter_ip,
       elk         = var.elk_count > 0 ? hcloud_floating_ip.bambielk_ip[0].ip_address : "127.0.0.1",
       engine      = var.engine_count > 0 ? hcloud_floating_ip.bambiengine_ip[0].ip_address : "127.0.0.1",
