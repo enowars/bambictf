31b3dd965da1630312c7e6b23fae31787e493864
Lucas Druschke <ldruschk@posteo.de>
2024-06-09 12:43:09 +0200

    start only the necessary OpenVPN servers on each router (untested)

--- configgen/configgen/gen_openvpn.py
+++ configgen/configgen/gen_openvpn.py
@@ -96,7 +96,7 @@ pull
 {config.client_key}</key>
 """
         # Save to disk
-        Path(f"{DATA_DIR}/export/ansible/routers/openvpn/team{team}.conf").write_text(
+        Path(f"{DATA_DIR}/export/ansible/routers/openvpn/router{(team - 1) % routers + 1}_team{team}.conf").write_text(
             server_config
         )
         Path(f"{DATA_DIR}/export/portal/team{team}/client.ovpn").write_text(
